204451167164d0203ff0e9be0f69bb32ba584cfe
SheetlSh3010 <sheetalshar3010@gmail.com>
2023-03-30 21:37:20 +0530

    index body

---
+++ frontend/components/navbar.js
@@ -0,0 +1,41 @@
+function navbar(){
+    return `
+    <div id="navbar1">
+    <div id="a">
+        <img src="" alt="">
+        <h4>Free shipping</h4>
+        <img src="" alt="">
+        <h4>Click & Connect</h4>
+        <img src="" alt="">
+        <h4>Return To Store</h4>
+    </div>
+    <div id="b">
+    <h4>Download Our Apps |</h4>
+    <h4>Store Locator |</h4>
+    <h4>Help</h4>
+    </div>
+</div>
+<div id="navbar2">
+
+    <img onclick='window.location.href="index.html"' src="https://i1.lmsin.net/website_images/in/logos/logo-lifestyle.svg" alt="">
+    <button onclick='window.location.href="women page.html"' >Women</button>
+    <button onclick='window.location.href="mens.html"'>Mens</button>
+    <button>Kids</button>
+    <button>Shoes&Bag</button>
+    <button>Beauty</button>
+    <input type="search" placeholder="What are you looking for?">
+    <p>More |</p>
+    <p onclick='window.location.href="./signin/signin.html"'>Sign up/Sign In</p>
+    <a href="cart.html">
+    <div id="id1">
+    <img src="https://img.freepik.com/free-psd/shopping-cart-3d-render-transparent-psd-file_460848-6771.jpg?size=626&ext=jpg&ga=GA1.2.609678927.1659701982" alt="">
+    </div>
+    </a>
+   
+    
+</div>
+<div id="navbar3">
+    <h4>Buy 1 Get 1 Free on Best of our Brands></h4>
+</div>`
+}
+export  {navbar};
